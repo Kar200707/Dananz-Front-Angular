e4dca448bba28bb49b47fc351ac368ab996e023c
Kar200707 <harutyunyan2007karen@gmail.com>
2023-10-02 13:04:11 +0400

    2023/10/02

---
+++ src/app/models/user-info.ts
@@ -0,0 +1,6 @@
+export interface UserInfo {
+  id: number,
+  userImg: string,
+  userName: string,
+  userBio: string
+}
--- src/app/pages/our-teams/our-teams.component.ts
+++ src/app/pages/our-teams/our-teams.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {ServicesBlockComponent} from "../../components/services-block/services-block.component";
+import {UserInfo} from "../../models/user-info";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-our-teams',
@@ -10,9 +12,60 @@ import {ServicesBlockComponent} from "../../components/services-block/services-b
   ],
   standalone: true,
   imports: [
-    ServicesBlockComponent
+    ServicesBlockComponent,
+    NgForOf,
+    NgIf
   ]
 })
 export class OurTeamsComponent {
-
+  userInfoData: UserInfo[] = [
+    {
+      id: 1,
+      userImg: 'assets/images/user1-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    },
+    {
+      id: 1,
+      userImg: 'assets/images/user2-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    },
+    {
+      id: 1,
+      userImg: 'assets/images/user3-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    },
+    {
+      id: 1,
+      userImg: 'assets/images/user4-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    },
+    {
+      id: 1,
+      userImg: 'assets/images/user5-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    },
+    {
+      id: 1,
+      userImg: 'assets/images/user6-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    },
+    {
+      id: 1,
+      userImg: 'assets/images/user7-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    },
+    {
+      id: 1,
+      userImg: 'assets/images/user8-our-teams.png',
+      userName: 'Shoo Phar Dhie',
+      userBio: 'CEO',
+    }
+  ]
 }
